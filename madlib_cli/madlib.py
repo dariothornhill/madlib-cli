1b8342032d575317df79b2b63c446c99142bf47c
Dario Thornhill <dario.thornhill@gmail.com>
2020-12-18 09:32:35 +0100

    Fixed reges bugs and completed program

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -2,36 +2,51 @@ import re
 from pathlib import Path
 
 def show_welcome():
-    print("*" * 36)
+    print("*" * 30)
     print("** Welcome to MadLib game! **")
-    print("*" * 36)
+    print("*" * 30)
     print("")
 
 def prompt_for_word(token: str) -> str :
-    pass
+    return input(f"Please enter a suitable {token}: ")
 
 def parse_template(template: str) ->  list:
-    tokens = re.findall("{[A-Za-z+]}", template)
+    tokens = re.findall(r"{([\w\s'\d\-]+)}", template)
     return tokens
 
 def load_template_file(filename: str) -> str :
     template_folder = Path('templates/')
-    print(template_folder)
     filepath = template_folder / filename
-    print(filepath)
     with open(filepath, "r") as file:
         template = file.read()
     return template
 
+def save_file(filename: str, madlib: str):
+    template_folder = Path('templates/')
+    filepath = template_folder / filename
+    with open(filepath, "w") as file:
+        file.write(madlib)
 
 def merge_template(template: str, words: list) -> str :
     result = template
     for word in words:
-        result = re.sub(r"{[\w]+}",word,result,1)
+        result = re.sub(r"{[\w\s'\d\-]+}",word,result,1)
     return result
 
 def main():
     show_welcome()
+    template = load_template_file('make_me_a_video_game.txt')
+    tokens = parse_template(template)
+    words = []
+    for token in tokens:
+        word = prompt_for_word(token)
+        words.append(word)
+    print("")
+    madlib = merge_template(template, words)
+    print(madlib)
+    save_file('madlib.txt', madlib)
+    print('Thank you! Come again!')
+    
 
 if __name__ == "__main__":
     main()
--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -1,5 +1,8 @@
-from madlib_cli.madlib import load_template_file, merge_template,parse_template
+import madlib_cli.madlib
+from madlib_cli.madlib import load_template_file, merge_template, parse_template, prompt_for_word, save_file
 import pytest
+import os
+from pathlib import Path
 
 def test_load_template_file():
     expected = 'Test File Data'
@@ -11,9 +14,11 @@ def test_load_template_file_should_error_when_no_file_found():
        load_template_file('nonexistant.txt')
 
 def test_parse_template():
-    expected = ['Adjective', 'Noun']
-    actual = parse_template('This is a {Adjective} {Noun}!')
+    expected = ['Adjective', 'A large animal', 'Noun', 'Number 1- 50', "A girl's name"]
+    actual = parse_template("This is a {Adjective} and large {A large animal} {Noun}! I will pay {Number 1- 50} dollars it, {A girl's name}")
+    print(actual)
     assert type(actual) is list
+    assert len(actual) == len(expected)
     assert all([a == b for a, b in zip(actual, expected)])
 
 def test_merge():
@@ -22,3 +27,24 @@ def test_merge():
     expected = 'This is a cool test!'
     actual = merge_template(template, words)
     assert expected == actual
+
+def test_prompt_for_word_return_input():
+    madlib_cli.madlib.input = lambda x: 'Table'
+    prompt_for_word('Noun') == 'Table'
+
+def test_save_file():
+    # setup
+    expected = 'Save File Data'
+    filename = 'save_test.txt'
+    filepath = Path('templates/') / filename
+    if os.path.exists(filepath):
+        os.remove(filepath)
+    assert not os.path.exists(filepath)
+    # test
+    save_file(filename, 'Save File Data')
+    actual = load_template_file(filename)
+    assert actual == expected 
+    # clean up
+    if os.path.exists(filepath):
+        os.remove(filepath)
+    assert not os.path.exists(filepath)
