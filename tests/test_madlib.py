98e0e7b67991bd093764229ef851e9b5bbba0464
Dario Thornhill <dario.thornhill@gmail.com>
2020-12-18 08:03:22 +0100

    Added load template and parse template tests

---
+++ tests/test_madlib.py
@@ -0,0 +1,17 @@
+from madlib_cli.madlib import load_template_file,parse_template
+import pytest
+
+def test_load_template_file():
+    expected = 'Test File Data'
+    actual = load_template_file('test.txt')
+    assert actual == expected 
+
+def test_load_template_file_should_error_when_no_file_found():
+    with pytest.raises(FileNotFoundError):
+       load_template_file('nonexistant.txt')
+
+def test_parse_template():
+    expected = ['Adjective', 'Noun']
+    actual = parse_template('This is a {Adjective} {Noun}!')
+    assert type(actual) is list
+    assert all([a == b for a, b in zip(actual, expected)])
